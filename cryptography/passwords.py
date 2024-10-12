9c6e7a5d65368c98551e94bfdbd8fb79b6bc9a9c
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-10-12 15:09:01 +0800

    Added pwgen functions

---
+++ cryptography/passwords.py
@@ -0,0 +1,23 @@
+from string import ascii_letters, digits
+import secrets
+import bcrypt
+
+async def generate_password(section_length = 6, number_of_sections = 3):
+    alphabet = ascii_letters + digits
+    sections = ''.join(secrets.choice(alphabet) for _ in range(section_length) for _ in range(number_of_sections))
+    password = '-'.join(sections)
+    return password
+
+async def generate_password_hash(password = None):
+    if password is None:
+        password = generate_password()
+
+    salt = bcrypt.gensalt() # generate salt to improve password security
+
+    hashed_password =  bcrypt.hashpw(password.encode('utf8'), salt)
+
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        generate_password_hash(password)
+
+    return hashed_password
+
