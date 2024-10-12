c2125e003217b0c6c6ea1749a080fef080b151c1
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-10-12 12:58:06 +0800

    Added load_dotenv function

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+# Database settings
+DB_NAME = os.getenv('DB_NAME')
+DB_HOST = os.getenv('DB_HOST')
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_PORT = os.getenv('DB_PORT')
 
 app = FastAPI() # initializing fastapi
 
