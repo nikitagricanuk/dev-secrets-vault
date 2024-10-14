d94992e51e8191b96047938682572672677abaf9
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-10-14 17:31:11 +0800

    Changed logic of working with DB, added get_user function

--- databases/db.py
+++ databases/db.py
@@ -1,34 +1,33 @@
 import psycopg2
+from psycopg2 import Error
 from config import DB_NAME, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_PORT
 from cryptography.passwords import generate_password, generate_password_hash
 
-try:
-    conn = psycopg2.connect(database=DB_NAME,
-                            host=DB_HOST,
-                            user=DB_USERNAME,
-                            password=DB_PASSWORD,
+def connect_db():
+    return psycopg2.connect(database=DB_NAME, user=DB_USERNAME, 
+                            password=DB_PASSWORD, host=DB_HOST, 
                             port=DB_PORT)
-except:
-    print("Error: Could not connect to the database")
-    conn = None
+
 
 async def create_user(username, email, role, password = None, extra_privileges = {}):
-    if conn is None:
-        return {"error": "No database connection"}
+    try:
+        db_connection = connect_db()
+        cursor = db_connection.cursor()
+    except(Error):
+        print("[Error]: ", Error)
     
     # Creating a user account
     if password is None:
         password = await generate_password()
     password_hash = await generate_password_hash(password)
 
-    with conn.cursor() as cursor:
-        try:
-            cursor.execute(
-                "INSERT INTO users (username, email, password_hash) VALUES (%s, %s, %s)",
-                (username, email, password_hash.decode('utf-8')) 
-            )
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
+    cursor.execute(
+        "INSERT INTO users (username, email, password_hash, is_disabled) VALUES (%s, %s, %s, %s)",
+        (username, email, password_hash.decode('utf-8'), False,) 
+    )
+
+    db_connection.commit()
+    db_connection.close()
 
     # Setting user role
     await set_user_role(username, role, extra_privileges)
@@ -38,8 +37,37 @@ async def create_user(username, email, role, password = None, extra_privileges =
             "role": role}
 
 async def set_user_role(username, role, extra_privileges = {}):
-    with conn.cursor() as cursor:
-        try:
-            cursor.execute(f"INSERT INTO roles (username, role_name, extra_privileges) VALUES (\'{username}\', \'{role}\', \'{extra_privileges}\')")
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
+    try:
+        db_connection = connect_db()
+        cursor = db_connection.cursor()
+    except(Error):
+        print("[Error]: ", Error)
+
+
+    cursor.execute(
+        "INSERT INTO roles (username, role_name, extra_privileges) VALUES (%s, %s, %s)",
+        (username, role, extra_privileges,)
+        )
+    
+    db_connection.commit()
+    db_connection.close()
+
+
+async def get_user(username):
+    try:
+        db_connection = connect_db()
+        cursor = db_connection.cursor()
+    except(Error):
+        print("[Error]: ", Error)
+    
+    cursor.execute(
+        "SELECT * FROM users WHERE username = %s",
+        (username,)
+    )
+
+    data = cursor.fetchone()
+
+    if data == []:
+        return False
+    
+    return data
