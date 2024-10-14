6e4a46930e3f384a91787ec6575a5eea56000e58
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-10-14 17:27:23 +0800

    Removed transit endpoints

--- api/api.py
+++ api/api.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 from api.auth import router as auth_router
 from api.secret import router as secret_router
 from api.sys import router as sys_router
-from api.transit import router as transit_router
 
 router = APIRouter() # Main router
 
@@ -10,4 +9,3 @@ router = APIRouter() # Main router
 router.include_router(auth_router)
 router.include_router(secret_router)
 router.include_router(sys_router)
-router.include_router(transit_router)
