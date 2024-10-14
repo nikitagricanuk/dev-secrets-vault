88d76e0cb147afd6eb98db4611486ec4924fefed
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-10-14 17:32:24 +0800

    Added entries for redis

--- config.py
+++ config.py
@@ -9,3 +9,8 @@ DB_HOST = os.getenv('DB_HOST')
 DB_USERNAME = os.getenv('DB_USERNAME')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_PORT = os.getenv('DB_PORT')
+
+JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PORT = os.getenv('REDIS_PORT')
