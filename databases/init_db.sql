bde534c3ba24d84d96abebee2c8355a6209a8358
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-10-16 21:28:51 +0800

    Removed roles table and andded setting and acl tables

--- databases/init_db.sql
+++ databases/init_db.sql
@@ -3,19 +3,33 @@ CREATE TABLE users (
     username VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    is_disabled BOOLEAN NOT NULL,
+    roles TEXT[] UNIQUE NOT NULL, -- roles the user belongs to, for example ('admin', 'dev', 'sysadmin')
+    is_disabled BOOLEAN NOT NULL DEFAULT false, -- set if the account is disabled
+    expires_at TIMESTAMP, -- if set, set is_disabled to true
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW()
 );
 
-CREATE TABLE roles (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    role_name VARCHAR(255) UNIQUE NOT NULL,
-);
-
 CREATE TABLE secrets (
     id SERIAL PRIMARY KEY,
-    group VARCHAR(255) NOT NULL,
-    sectet_data jsonb NOT NULL
+    secret_data jsonb NOT NULL
+);
+
+CREATE TABLE settings (
+    id SERIAL PRIMARY KEY,
+    scope TEXT NOT NULL, -- settings scope, for example global for all users, or user:uid for specific user
+    setting_key TEXT NOT NULL, -- store data in key - value format, like in redis
+    setting_value JSONB NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE acl (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    resource_type TEXT NOT NULL,
+    resource_id INT NOT NULL,
+    permission TEXT[] NOT NULL, -- for example ('read', 'write')
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW()
 );
