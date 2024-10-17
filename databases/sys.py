83b3f2501eb1213e58b295f49c38a82d55613df9
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-10-17 19:40:58 +0800

    Added create_setting and get_settings funcs

---
+++ databases/sys.py
@@ -0,0 +1,44 @@
+from . import connect_db
+from psycopg2 import Error
+import json
+
+async def create_setting(scope_type: str, scope: str, setting_key: str,
+                             setting_value: str):
+    try:
+        db_connection = connect_db()
+        cursor = db_connection.cursor()
+    except(Error):
+        print("[Error]: ", Error)
+
+    cursor.execute(
+        """
+        INSERT INTO settings (scope_type, scope, setting_key, setting_value) 
+        VALUES (%s, %s, %s, %s);
+        """, (scope_type, scope, setting_key, setting_value,)
+    )
+
+    db_connection.commit()
+    db_connection.close()
+
+async def get_all_settings():
+    try:
+        db_connection = connect_db()
+        cursor = db_connection.cursor()
+    except(Error):
+        print("[Error]: ", Error)
+
+    
+
+async def get_setting(setting_key: str):
+    try:
+        db_connection = connect_db()
+        cursor = db_connection.cursor()
+    except(Error):
+        print("[Error]: ", Error)
+
+    cursor.execute(
+        "SELECT * FROM settings WHERE setting_key = %s;",
+        (setting_key,)
+    )
+
+    
