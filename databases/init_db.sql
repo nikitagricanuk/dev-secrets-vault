0763c9f05d8dc2ade3187fd9c7e5d5cb79dbdca0
Veselove <vault@msksrv.nodes.nikitagricanuk.ru>
2024-10-20 12:23:35 +0000

    Изменен тип возвращаемых данных процедурой create_secret

--- databases/init_db.sql
+++ databases/init_db.sql
@@ -7,11 +7,11 @@ CREATE TABLE users (
     email VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(60) NOT NULL,
     roles TEXT[] UNIQUE NOT NULL, -- roles the user belongs to, for example ('admin', 'dev', 'sysadmin')
-    is_disabled BOOLEAN NOT NULL DEFAULT false, -- set if the account is disabled
+    is_disabled BOOLEAN NOT NULL DEFAULT false, -- SET IF the account is disabled
     is_admin BOOLEAN NOT NULL DEFAULT false,
-    expires_at TIMESTAMP, -- if set, set is_disabled to true
+    expires_at TIMESTAMP, -- IF SET, SET is_disabled to true
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
-    updated_at TIMESTAMP DEFAULT NOW()
+    UPDATEd_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE secrets (
@@ -22,23 +22,23 @@ CREATE TABLE secrets (
     secret_data TEXT NOT NULL,
     secret_data_history TEXT,
     secret_data_hashsum TEXT,
-    is_disabled BOOLEAN NOT NULL DEFAULT false, -- set if the account is disabled
+    is_disabled BOOLEAN NOT NULL DEFAULT false, -- SET IF the account is disabled
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     created_by VARCHAR(50) NOT NULL, -- username
-    expires_at TIMESTAMP, -- if set, set is_disabled to true
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    expires_at TIMESTAMP, -- IF SET, SET is_disabled to true
+    UPDATEd_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE settings (
+CREATE TABLE SETtings (
     id SERIAL PRIMARY KEY,
     scope_type TEXT NOT NULL, -- type of scope, for example 'global' or 'user'
-    scope UUID, -- settings scope, only use if scope_type is anything but global
-    setting_key TEXT NOT NULL, -- store data in key - value format, like in redis
-    setting_value jsonb NOT NULL,
-    is_disabled BOOLEAN NOT NULL DEFAULT false, -- set if the account is disabled
+    scope UUID, -- SETtings scope, only use IF scope_type is anything but global
+    SETting_key TEXT NOT NULL, -- store data in key - value format, like in redis
+    SETting_value jsonb NOT NULL,
+    is_disabled BOOLEAN NOT NULL DEFAULT false, -- SET IF the account is disabled
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    expires_at TIMESTAMP, -- if set, set is_disabled to true,
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    expires_at TIMESTAMP, -- IF SET, SET is_disabled to true,
+    UPDATEd_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE acl (
@@ -46,17 +46,17 @@ CREATE TABLE acl (
     resource_type TEXT NOT NULL, -- for example 'secret'
     resource_id INT NOT NULL,
     permission TEXT[] NOT NULL, -- for example ('read', 'write')
-    is_disabled BOOLEAN NOT NULL DEFAULT false, -- set if the account is disabled
+    is_disabled BOOLEAN NOT NULL DEFAULT false, -- SET IF the account is disabled
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
-    expires_at TIMESTAMP, -- if set, set is_disabled to true,
-    updated_at TIMESTAMP DEFAULT NOW()
+    expires_at TIMESTAMP, -- IF SET, SET is_disabled to true,
+    UPDATEd_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE userkeys (
   username varchar(50),
-  crypto_key text,
+  crypto_key TEXT,
   created_at TIMESTAMP DEFAULT NOW() NOT NULL,
-  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+  UPDATEd_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE secret_history (
@@ -66,7 +66,7 @@ CREATE TABLE secret_history (
   tags TEXT[] NOT NULL, -- for example 'api_key' or 'password'
   secret_data TEXT NOT NULL,
   secret_data_hashsum TEXT,
-  updated_at TIMESTAMP DEFAULT NOW()
+  UPDATEd_at TIMESTAMP DEFAULT NOW()
 );
 
 -- name, description, tags, secret_data, created_by, expires_at
@@ -77,26 +77,28 @@ CREATE FUNCTION create_secret(
     _tags TEXT[],
     _secret_data jsonb,
     _created_by VARCHAR(50), -- username
-    _expires_at TIMESTAMP -- if set, set is_disabled to true
-    ) RETURNS BOOLEAN
+    _expires_at TIMESTAMP -- IF SET, SET is_disabled to true
+    ) RETURNS UUID
  LANGUAGE plpgsql AS
 $$
 DECLARE
-   _some_key text;
-   _scrypd text;
-   _hashsum text;
+   _id UUID;
+   _some_key TEXT;
+   _scrypd TEXT;
+   _hashsum TEXT;
 BEGIN
-  if EXISTS(select crypto_key from userkeys where username = _created_by) then
-    select crypto_key from userkeys where username = _created_by into _some_key;
-  else
+  IF EXISTS(SELECT crypto_key FROM userkeys WHERE username = _created_by) THEN
+    SELECT crypto_key FROM userkeys WHERE username = _created_by INTO _some_key;
+  ELSE
     _some_key := gen_random_bytes(16);
-    insert into userkeys values (_created_by, _some_key);
-  end if;
-  _scrypd := pgp_sym_encrypt(cast(_secret_data as text), _some_key);
+    INSERT INTO userkeys VALUES (_created_by, _some_key);
+  END IF;
+  _scrypd := pgp_sym_encrypt(cast(_secret_data as TEXT), _some_key);
   _hashsum := md5(_scrypd);
-  insert into secrets (name, description, tags, secret_data, secret_data_hashsum, created_by, expires_at) 
-    values (_name, _description, _tags, _scrypd, _hashsum, _created_by, _expires_at);
-  RETURN TRUE;
+  INSERT INTO secrets (name, description, tags, secret_data, secret_data_hashsum, created_by, expires_at) 
+    VALUES (_name, _description, _tags, _scrypd, _hashsum, _created_by, _expires_at)
+  RETURNING id INTO _id;
+  RETURN _id;
 END
 $$;
 
@@ -111,11 +113,11 @@ CREATE FUNCTION get_secret(
     secret_data TEXT,
     secret_data_history TEXT,
     secret_data_hashsum TEXT,
-    is_disabled BOOLEAN, -- set if the account is disabled
+    is_disabled BOOLEAN, -- SET IF the account is disabled
     created_at TIMESTAMP,
     created_by VARCHAR(50), -- username
-    expires_at TIMESTAMP, -- if set, set is_disabled to true
-    updated_at TIMESTAMP
+    expires_at TIMESTAMP, -- IF SET, SET is_disabled to true
+    UPDATEd_at TIMESTAMP
 )
   LANGUAGE plpgsql AS
 $$
@@ -124,42 +126,42 @@ BEGIN
 END;
 $$;
 
-CREATE FUNCTION update_secret(
+CREATE FUNCTION UPDATE_secret(
     _id UUID,
     _name VARCHAR(50),
     _description TEXT,
     _tags TEXT[],
     _secret_data jsonb,
     _created_by VARCHAR(50), -- username
-    _expires_at TIMESTAMP -- if set, set is_disabled to true
+    _expires_at TIMESTAMP -- IF SET, SET is_disabled to true
     ) RETURNS BOOLEAN
  LANGUAGE plpgsql AS
 $$
 DECLARE
-   _some_key text;
-   _scrypd text;
-   _hashsum text;
+   _some_key TEXT;
+   _scrypd TEXT;
+   _hashsum TEXT;
 BEGIN
-  IF _id IS NULL AND _name IS NULL then
+  IF _id IS NULL AND _name IS NULL THEN
     RETURN FALSE;
   END IF;
 
-  if EXISTS(select crypto_key from userkeys where username = _created_by) then
-    select crypto_key from userkeys where username = _created_by into _some_key;
-  else
+  IF EXISTS(SELECT crypto_key FROM userkeys WHERE username = _created_by) THEN
+    SELECT crypto_key FROM userkeys WHERE username = _created_by INTO _some_key;
+  ELSE
     _some_key := gen_random_bytes(16);
-    insert into userkeys values (_created_by, _some_key);
-  end if;
+    INSERT INTO userkeys VALUES (_created_by, _some_key);
+  END IF;
   
   INSERT INTO secret_history (id, name, description, tags, secret_data, secret_data_hashsum)
-  select id, name, description, tags, secret_data, secret_data_hashsum from secrets
-  where secrets.id = _id OR secrets.name = name;
+  SELECT id, name, description, tags, secret_data, secret_data_hashsum FROM secrets
+  WHERE secrets.id = _id OR secrets.name = name;
   
-  _scrypd := pgp_sym_encrypt(cast(_secret_data as text), _some_key);
+  _scrypd := pgp_sym_encrypt(cast(_secret_data as TEXT), _some_key);
   _hashsum := md5(_scrypd);
-  update secrets set (name, description, tags, secret_data, secret_data_hashsum, created_by, expires_at) 
+  UPDATE secrets SET (name, description, tags, secret_data, secret_data_hashsum, created_by, expires_at) 
     = (_name, _description, _tags, _scrypd, _hashsum, _created_by, _expires_at)
-    where secrets.name = _name OR secrets.id = _id;
+    WHERE secrets.name = _name OR secrets.id = _id;
   
   RETURN TRUE;
 END
