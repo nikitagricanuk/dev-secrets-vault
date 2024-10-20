76a8b2c1b70476755250398a1082dc2303651633
MegaAndrey <vesolandrey@gmail.com>
2024-10-21 01:47:55 +0800

    Добавлен маршрут и логика для обновления секрета, реализована проверка доступа на запись чтение секрета на уровне БД

--- api/secret.py
+++ api/secret.py
@@ -32,3 +32,9 @@ async def api_get_all_secrets(session_data: str = Depends(authenticate_user_toke
 @router.delete("/v1/secrets/{secret_id}")
 async def api_delete_secret(secret_id: str, session_data: str = Depends(authenticate_user_token)):
     return await delete_secret(secret_id)
+
+@router.post("/v1/secrets/update/{secret_id}")
+async def api_secret_update(secret_id: str, secret_data: Secret, session_data: str = Depends(authenticate_user_token)):
+    return await upadte_secret(secret_id, secret_data.name, secret_data.data, 
+                        secret_data.tags, session_data["username"],
+                        secret_data.ttl, secret_data.description)
