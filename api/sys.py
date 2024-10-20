86f97e9f9e1f6e4482af71cd9f3faffaf4b75051
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-10-20 15:12:22 +0800

    Added settings support: get one, get all, set

--- api/sys.py
+++ api/sys.py
@@ -1,11 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from security.settings import get_settings_list, get_setting, set_setting
+from api.auth import authenticate_user_token
 
 router = APIRouter()
 
-@router.post("/v1/sys/security")
-async def api_auth_methods():
-    return {"message": "test"}
+class Setting(BaseModel):
+    key: str
+    value: str
 
-@router.post("/v1/sys/acl")
-async def api_auth_methods():
-    return {"message": "test"}
+@router.get("/v1/sys/settings")
+async def api_set_settings_list(session_data: str = Depends(authenticate_user_token)):
+    return await get_settings_list()
+
+@router.get("/v1/sys/settings/{setting_key}")
+async def api_get_setting(setting_key: str, session_data: str = Depends(authenticate_user_token)):
+    return await get_setting(setting_key)
+
+@router.post("/v1/sys/settings")
+async def api_set_setting(setting: Setting, session_data: str = Depends(authenticate_user_token)):
+    return await set_setting(setting.key, setting.value)
--- security/settings.py
+++ security/settings.py
@@ -1,10 +1,101 @@
 import json
 from config import PATH_TO_SETTINGS_FILE
 from fastapi.responses import JSONResponse
+import os
 
-
-async def get_settings():
+async def get_settings_list():
     with open(PATH_TO_SETTINGS_FILE, 'r') as fd:
         settings = json.load(fd) 
 
-    return JSONResponse(content=settings)
+    settings_list = []
+
+    # Iterate over the settings array to collect all settings
+    for section in settings["settings"]:
+        for category, items in section.items():  # Iterate over categories (e.g., 'auth', 'security')
+            for item in items:  # Each item is a setting inside the category
+                for setting_key, setting_data in item.items():  # Access the setting name and its data
+                    settings_list.append({
+                        "setting_key": f"{category}.{setting_key}",  # Format: "category.setting"
+                        "value": setting_data["value"],
+                        "comment": setting_data["comment"]
+                    })
+    return JSONResponse(content=settings_list)
+
+async def get_setting(setting_key: str):
+    """
+    Get a setting from the settings if it exists
+    
+    Args:
+        setting_key (str): The name of the setting in the following format: category.setting
+        
+    Returns:
+        int: A status code indicating if any error occurred:
+            - 0: Setting does not exist.
+        dict: All good, just return the setting:
+            - data: The setting key and value.
+    """
+    with open(PATH_TO_SETTINGS_FILE, 'r') as fd:
+        settings = json.load(fd)
+
+    category = setting_key.split('.')[0]
+    setting = setting_key.split('.')[1]
+
+    # Iterate over the settings array to collect all settings
+    return_data = None
+    for section in settings["settings"]:
+        if category in section: # Iterate over categories (e.g., 'auth', 'security')
+            for item in section[category]:
+                if setting in item: # Iterate over category items
+                    value = item[setting]["value"]
+                    comment = item[setting]["comment"]
+                    return {"setting_key": setting_key, 
+                            "value": value,
+                            "comment": comment}
+
+    return 2
+
+async def set_setting(key: str, value: str):
+    if await validate_setting(key, value):
+        with open(PATH_TO_SETTINGS_FILE, 'r') as fd:
+            settings = json.load(fd)
+
+        category = key.split('.')[0]
+        setting = key.split('.')[1]
+
+        # Iterate over the settings array to collect all settings
+        return_data = None
+        for section in settings["settings"]:
+            if category in section: # Iterate over categories (e.g., 'auth', 'security')
+                for item in section[category]:
+                    if setting in item: # Iterate over category items
+                        item[setting]["value"] = value
+                        break
+        
+        with open(PATH_TO_SETTINGS_FILE, 'w') as fd:
+            json.dump(settings, fd, indent=2)
+
+        return_data =  {"setting_key": key, 
+                        "value": value
+                        }
+
+        return JSONResponse(content=return_data)
+    
+    return_data = {"detail": "The provided setting value is invalid."}
+    return JSONResponse(content=return_data)
+
+async def validate_setting(key: str, value: str):
+    with open(PATH_TO_SETTINGS_FILE, 'r') as fd:
+        settings = json.load(fd)
+    try:
+        category = key.split('.')[0]
+        setting = key.split('.')[1]
+    except IndexError:
+        return False
+
+    for section in settings["settings"]:
+        if category in section: # Iterate over categories (e.g., 'auth', 'security')
+            for item in section[category]:
+                if setting in item: # Iterate over category items
+                    return True
+                
+    return False
