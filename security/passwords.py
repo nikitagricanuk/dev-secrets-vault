0b8321506c6bd5d88cebbfcafad27c44ce968b9e
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-10-25 00:31:02 +0800

    Fixed pwgen func

--- security/passwords.py
+++ security/passwords.py
@@ -1,14 +1,32 @@
-from string import ascii_letters, digits
-import secrets
+import string
+import random
 import bcrypt
 
-async def generate_password(section_length = 6, number_of_sections = 3):
-    alphabet = ascii_letters + digits
-    sections = [] # TODO: дописать генерацию паролей
-    for i in range(number_of_sections):
-        for i in range(section_length):
-            sections[0] += i
-    password = '-'.join(sections)
+async def generate_password(length=12, use_digits=True, use_uppercase=True, use_lowercase=True, use_special_chars=False):
+    """
+    Generates a random password based on the provided criteria.
+    
+    :param length: Length of the password (default 12)
+    :param use_digits: Include digits in the password (default True)
+    :param use_uppercase: Include uppercase letters in the password (default True)
+    :param use_lowercase: Include lowercase letters in the password (default True)
+    :param use_special_chars: Include special characters in the password (default True)
+    :return: Randomly generated password as a string
+    """
+    characters = ''
+    if use_digits:
+        characters += string.digits
+    if use_uppercase:
+        characters += string.ascii_uppercase
+    if use_lowercase:
+        characters += string.ascii_lowercase
+    if use_special_chars:
+        characters += string.punctuation
+    
+    if not characters:
+        raise ValueError("At least one character set must be enabled.")
+
+    password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
 async def generate_password_hash(password = None):
