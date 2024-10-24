db47dc33bd1178f1469bb0d45ee12bbcf759517a
MegaAndrey <vesolandrey@gmail.com>
2024-10-24 23:51:08 +0800

    acl logic

--- api/secret.py
+++ api/secret.py
@@ -39,4 +39,5 @@ async def api_delete_secret(secret_id: str, session_data: str = Depends(authenti
 async def api_secret_update(secret_id: str, secret_data: Secret, session_data: str = Depends(authenticate_user_token)):
     return await update_secret(secret_id, secret_data.name, secret_data.data, 
                         secret_data.tags, session_data["username"],
-                        secret_data.ttl, secret_data.description)
+                        secret_data.ttl, secret_data.description,
+                        session_data=session_data, action='update', resource_id=secret_id)
