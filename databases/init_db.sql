c686d89a474d2f872c4b4f74ff5990491f01805d
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-10-25 15:29:13 +0800

    Deleted legacy table settings

--- databases/init_db.sql
+++ databases/init_db.sql
@@ -29,18 +29,6 @@ CREATE TABLE secrets (
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE settings (
-    id SERIAL PRIMARY KEY,
-    scope_type TEXT NOT NULL, -- type of scope, for example 'global' or 'user'
-    scope UUID, -- settings scope, only use if scope_type is anything but global
-    setting_key TEXT NOT NULL, -- store data in key - value format, like in redis
-    setting_value jsonb NOT NULL,
-    is_disabled BOOLEAN NOT NULL DEFAULT false, -- set if the account is disabled
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    expires_at TIMESTAMP, -- if set, set is_disabled to true,
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
 CREATE TABLE acl (
     id SERIAL PRIMARY KEY,
     resource_type TEXT NOT NULL, -- for example 'secret', 'user'
