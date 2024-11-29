2f3fcd78abed57fac79b5dfe93529f7714ce64f3
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2024-11-30 01:39:09 +0800

    Added logging init

--- logging_config.py
+++ logging_config.py
@@ -6,7 +6,7 @@ def setup_logger(name):
     if not logger.handlers:  # Prevent adding multiple handlers in re-imports
         handler = logging.StreamHandler()
         formatter = logging.Formatter(
-            "[%(asctime)s] %(levelname)-8s %(name)s: %(message)s"
+            "%(levelname)s:     %(asctime)s, %(name)s: %(message)s"
         )
         handler.setFormatter(formatter)
         logger.addHandler(handler)
